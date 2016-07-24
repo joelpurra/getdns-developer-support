703e3ca771c338fc3c315b7156cc56ebaf0e7e8f
Joel Purra <code+github@joelpurra.com>
2016-07-24 21:43:25 +0200

    Add getdns-node testing against different versions of node

---
+++ src/getdns-node/node-version-builds/README.md
@@ -0,0 +1,20 @@
+# Semi-automated compilation and testing of [getdns-node](https://github.com/getdnsapi/getdns-node) against different versions of [`node`](https://nodejs.org/)
+
+Compiles/builds/installs using `npm install` and subsequently runs `npm test` of [`getdns-node`](https://github.com/getdnsapi/getdns-node) against different versions of [`node`](https://nodejs.org/). This way compatibility can be confirmed, and possibly incompatibilities found.
+
+## Requirements
+
+- [`n`](https://github.com/tj/n) for node version management.
+
+## Usage
+
+`node-version-builds.sh <git repository> <commitish>`
+
+- Relies on the environment variable `GETDNS_NODE_TARGET` being set, as it's used for output directories and log naming.
+- Pass a git repository and a commitish (commit hash, tag or branch name) to have it automatically cloned and/or cleaned before the build.
+- The subfolder `getdns-node-builds/` will contain separate build folders and log output from `npm install` and `npm test`.
+
+
+---
+
+Copyright (c) 2014, 2015, 2016 [Joel Purra](http://joelpurra.com/). Released under the [BSD 3-Clause License](https://opensource.org/licenses/BSD-3-Clause). Part of [`getdns`](https://getdnsapi.net/) developer support library [getdns-support](https://github.com/joelpurra/getdns-support)
---
+++ src/getdns-node/node-version-builds/node-version-builds.sh
@@ -0,0 +1,145 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+set -o pipefail
+set -x
+
+externalExecutableExists() {
+	local executable="$1"
+
+	if builtin type "$executable" &>/dev/null;
+	then
+		return 0
+	else
+		return 1
+	fi
+}
+
+function crossplatformReadlink {
+	# http://stackoverflow.com/questions/1055671/how-can-i-get-the-behavior-of-gnus-readlink-f-on-a-mac
+	# http://stackoverflow.com/a/1116890
+	TARGET_FILE="$1"
+
+	cd `dirname $TARGET_FILE`
+	TARGET_FILE=`basename $TARGET_FILE`
+
+	# Iterate down a (possible) chain of symlinks
+	while [ -L "$TARGET_FILE" ]
+	do
+	    TARGET_FILE=`readlink $TARGET_FILE`
+	    cd `dirname $TARGET_FILE`
+	    TARGET_FILE=`basename $TARGET_FILE`
+	done
+
+	# Compute the canonicalized name by finding the physical path
+	# for the directory we're in and appending the target file.
+	PHYS_DIR=`pwd -P`
+	RESULT=$PHYS_DIR/$TARGET_FILE
+	echo "$RESULT"
+}
+
+# Must be executed in this file.
+# Used to recursively call np and to find files to `source`.
+THIS_SOURCE=$(crossplatformReadlink "$BASH_SOURCE")
+
+THIS_SOURCE_DIR="${THIS_SOURCE%/*}"
+
+BUILD_ROOT_DIR="${THIS_SOURCE_DIR}/getdns-node-builds"
+
+if [[ -z "$GETDNS_TARGET" ]];
+then
+    echo "No GETDNS_TARGET defined" >&2
+    exit 1
+fi
+
+GIT_REPOSITORY="${1:-}"
+shift
+
+if [[ -z "$GIT_REPOSITORY" ]];
+then
+    echo "No outside git repository specified: ${GIT_REPOSITORY}" >&2
+    exit 1
+fi
+
+GIT_COMMITISH="${1:-}"
+shift
+
+if [[ -z "$GIT_COMMITISH" ]];
+then
+    echo "No outside git commit(ish) specified: ${GIT_COMMITISH}" >&2
+    exit 1
+fi
+
+if ! externalExecutableExists "n";
+then
+    echo "Could not find command in \$PATH: n" >&2
+    exit 1
+fi
+
+mkdir -p "$BUILD_ROOT_DIR"
+
+NODE_TARGETS=("0.12.0" "0.12.15" "4.0.0" "4.4.7" "5.0.0" "5.12.0" "6.0.0" "6.3.1")
+
+ORIGINAL_NODE_VERSION=$(node --version)
+
+onExit() {
+    # Restore node version
+    n $ORIGINAL_NODE_VERSION
+}
+
+{ trap 'onExit' EXIT; }
+
+for NODE_TARGET in ${NODE_TARGETS[@]};
+do
+(
+    echo "Building getdns ${GETDNS_TARGET} in node {$NODE_TARGET}"
+
+    pushd "$BUILD_ROOT_DIR" >/dev/null
+    (
+        # Gah. Didn't think it would be this much hassle.
+        # export NVM_DIR="$HOME/.nvm"
+        # [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
+        #
+        # # Node version might already be installed, but whatever.
+        # nvm install $NODE_TARGET
+        # nvm use $NODE_TARGET
+
+        # Install/use the target node version.
+        # Requires an existing installation ;)
+        # https://github.com/tj/n
+        # Revert first, to "guarantee" a working version
+        n $ORIGINAL_NODE_VERSION
+
+        # npm --silent install n
+        # ./node_modules/.bin/n $NODE_TARGET
+        n $NODE_TARGET
+
+        NODE_TARGET_EXACT=$(node --version)
+
+        echo "Using node $(node --version) and npm $(npm --version)"
+
+        GETDNS_NODE_TARGET="getdns-${GETDNS_TARGET}-node-${NODE_TARGET_EXACT}"
+
+        mkdir -p "node-$GETDNS_NODE_TARGET"
+
+        pushd "node-$GETDNS_NODE_TARGET" >/dev/null
+        (
+            git clone "$GIT_REPOSITORY" "." || git fetch --all --tags --prune
+            git checkout --force "$GIT_COMMITISH"
+            git clean -fdx
+
+            npm --silent install |& tee ../$GETDNS_NODE_TARGET.npm-install.log || { echo "npm install failed" >&2 ; continue; }
+
+            # Using a locally installed node-gyp.
+            npm --silent install node-gyp
+            ./node_modules/.bin/node-gyp configure --debug --verbose
+            ./node_modules/.bin/node-gyp rebuild --debug --jobs 6 --verbose
+
+            npm --silent test |& tee ../$GETDNS_NODE_TARGET.npm-test.log || { echo "npm test failed" >&2 ; continue; }
+        )
+        popd >/dev/null
+    )
+    popd >/dev/null
+)
+done;
